1bb3471167fb06e6f1f9840f9c00a5881c31170d
deep-explorer <explorer0933@gmail.com>
2024-04-19 14:36:40 +0200

    update: updated auto scroll bottom while chatting

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,7 +5,7 @@ import { ChatList } from '@/components/chat-list'
 import { ChatPanel } from '@/components/chat-panel'
 import { EmptyScreen } from '@/components/empty-screen'
 import { useLocalStorage } from '@/lib/hooks/use-local-storage'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useUIState, useAIState } from 'ai/rsc'
 import { Session } from '@/lib/types'
 import { usePathname, useRouter } from 'next/navigation'
@@ -24,6 +24,7 @@ export interface ChatProps extends React.ComponentProps<'div'> {
 export function Chat({ id, className, session, missingKeys }: ChatProps) {
   const router = useRouter()
   const path = usePathname()
+  const chatContainerRef = useRef<HTMLDivElement>(null)
   const [input, setInput] = useState('')
   const [messages] = useUIState()
   const [aiState] = useAIState()
@@ -37,6 +38,12 @@ export function Chat({ id, className, session, missingKeys }: ChatProps) {
       }
     }
   }, [id, path, session?.user, messages])
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
+    }
+  }, [messages])
 
   useEffect(() => {
     const messagesLength = aiState.messages?.length
@@ -57,9 +64,12 @@ export function Chat({ id, className, session, missingKeys }: ChatProps) {
 
   return (
     <>
-      <div className="relative flex justify-center h-[calc(100vh_-_theme(spacing.36))] ">
+      <div className="relative flex justify-center h-[calc(100vh_-_theme(spacing.28))] ">
         <div className="max-w-[940px] min-w-[350px] w-full flex">
-          <div className="group w-full overflow-auto  bg-[#CDE2E7] dark:bg-[#071920] rounded-lg border-black dark:border-white border-2">
+          <div
+            className="group w-full overflow-auto  bg-[#CDE2E7] dark:bg-[#071920] rounded-lg border-black dark:border-white border-2"
+            ref={chatContainerRef}
+          >
             <div className={cn('pt-4 md:pt-10 ', className)}>
               {messages.length ? (
                 <ChatList
@@ -79,7 +89,7 @@ export function Chat({ id, className, session, missingKeys }: ChatProps) {
       <div className="w-full flex justify-center space-y-4 border-t bg-background p-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
         <div className="max-w-[940px] w-full flex flex-col gap-2">
           <PromptForm input={input} setInput={setInput} />
-          <PromptUsageWidget />
+          {/* <PromptUsageWidget /> */}
         </div>
       </div>
     </>

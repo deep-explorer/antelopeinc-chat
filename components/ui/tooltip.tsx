beeb81df190e788e89ff07a5b0308a2da26595d2
deep-explorer <explorer0933@gmail.com>
2024-05-07 23:11:47 +0200

    fix: fixed tooltip size

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -48,7 +48,7 @@ export function PrimaryTooltip({ trigger, description }: PrimaryTooltipProps) {
           <BasicTooltip.Content
             side="bottom"
             align="end"
-            className="max-w-[183px] data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-primary p-[10px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] text-[10px] font-extralight"
+            className="max-w-[200px] data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-primary p-[10px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] text-sm font-extralight"
             sideOffset={4}
             style={{
               transform: 'translateX(8px)',

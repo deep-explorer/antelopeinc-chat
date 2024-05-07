89bfba836fb7332d55da3b4a343380020c7c955c
deep-explorer <explorer0933@gmail.com>
2024-05-08 01:35:58 +0200

    fix: fixed navbar fading

--- components/header.tsx
+++ components/header.tsx
@@ -80,7 +80,7 @@ export function Header() {
   const [isScrolled, setIsScrolled] = React.useState(false)
   React.useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > (windowWidth > 768 ? 300 : 180)) {
+      if (window.scrollY > (windowWidth > 768 ? 350 : 220)) {
         setIsScrolled(true)
       } else {
         setIsScrolled(false)
@@ -93,91 +93,87 @@ export function Header() {
   return (
     <>
       <header
-        className={`${isScrolled ? 'h-[48px] md:h-[80px]' : 'h-[220px] md:h-[320px]'}`}
+        className={`scrolled p-4 lg:px-12 flex items-center justify-between w-full bg-[#122830] overflow-hidden ease-in-out transition-all duration-1000 shadow-2xl ${isScrolled ? 'h-[48px] md:h-[80px]' : 'h-0 py-0'}`}
       >
-        {isScrolled ? (
+        {isScrolled && (
           <>
-            <div className="scrolled p-4 lg:px-12 lg:py-4 flex items-center justify-between w-full bg-[#122830] shadow-2xl">
-              <div className="flex items-center">
-                <Link href={companyUrl} rel="nofollow" className="flex gap-2">
-                  <Image
-                    src={`/header-logo.png`}
-                    alt="logo"
-                    width={32}
-                    height={32}
-                  />
-                  <h2 className="text-xl">Antelope</h2>
-                </Link>
-              </div>
-              <div className="hidden lg:flex gap-2 items-center">
-                <div>
-                  <Image
-                    src={`/vitamin/logos/renzo.png`}
-                    alt="renzologo"
-                    width={42}
-                    height={42}
-                  />
-                </div>
-                <p className="text-3xl font-semibold">{title?.title}</p>
-              </div>
-              <div>
-                {/* <ThemeToggle /> */}
-                <Button
-                  onClick={() => window.open(companyUrl, '_blank')}
-                  size={'2'}
-                >
-                  Get in touch
-                </Button>
-              </div>
+            <div className="flex items-center">
+              <Link href={companyUrl} rel="nofollow" className="flex gap-2">
+                <Image
+                  src={`/header-logo.png`}
+                  alt="logo"
+                  width={32}
+                  height={32}
+                />
+                <h2 className="text-xl">Antelope</h2>
+              </Link>
             </div>
-          </>
-        ) : (
-          <>
-            <div className="p-4 lg:px-12 lg:py-8 flex items-center justify-between w-full bg-[#122830]  shadow-2xl">
-              <div className="flex items-center">
-                <Link href={companyUrl} rel="nofollow" className="flex gap-2">
-                  <Image
-                    src={`/header-logo.png`}
-                    alt="logo"
-                    width={32}
-                    height={32}
-                  />
-                  <h2 className="text-xl">Antelope</h2>
-                </Link>
-              </div>
-              <div className="hidden lg:flex items-center justify-end space-x-2 gap-12 text-lg">
-                <Link href={`${companyUrl}/#banner`}>Home</Link>
-                <Link href={`${companyUrl}/#benefits`}>What We Do</Link>
-                <Link href={`${companyUrl}/case-study`}>Our Reports</Link>
-                <Link href={`${companyUrl}/#process`}>Process</Link>
-                <Link href={`${companyUrl}/blog/`}>Blog</Link>
-              </div>
+            <div className="hidden lg:flex gap-2 items-center">
               <div>
-                {/* <ThemeToggle /> */}
-                <Button
-                  onClick={() => window.open(companyUrl, '_blank')}
-                  size={'2'}
-                >
-                  Get in touch
-                </Button>
+                <Image
+                  src={`/vitamin/logos/renzo.png`}
+                  alt="renzologo"
+                  width={42}
+                  height={42}
+                />
               </div>
+              <p className="text-3xl font-semibold">{title?.title}</p>
             </div>
-            <div className="text-center pt-8 md:pt-[64px] px-6 flex flex-col gap-4 md:gap-6">
-              <h1
-                className="text-xs md:text-base text-primary font-bold"
-                style={{ letterSpacing: 2 }}
+            <div>
+              {/* <ThemeToggle /> */}
+              <Button
+                onClick={() => window.open(companyUrl, '_blank')}
+                size={'2'}
               >
-                ANTELOPE CHATBOT
-              </h1>
-              <h2 className="text-xl md:text-3xl font-bold">{title?.title}</h2>
-              <p className="text-[#B9CAD0] text-sm md:text-lg">
-                {title?.description}
-              </p>
+                Get in touch
+              </Button>
             </div>
           </>
         )}
       </header>
-      {isScrolled && <div className="h-20"></div>}
+      <header>
+        <div className="p-4 lg:px-12 lg:py-8 flex items-center justify-between w-full bg-[#122830]  shadow-2xl">
+          <div className="flex items-center">
+            <Link href={companyUrl} rel="nofollow" className="flex gap-2">
+              <Image
+                src={`/header-logo.png`}
+                alt="logo"
+                width={32}
+                height={32}
+              />
+              <h2 className="text-xl">Antelope</h2>
+            </Link>
+          </div>
+          <div className="hidden lg:flex items-center justify-end space-x-2 gap-12 text-lg">
+            <Link href={`${companyUrl}/#banner`}>Home</Link>
+            <Link href={`${companyUrl}/#benefits`}>What We Do</Link>
+            <Link href={`${companyUrl}/case-study`}>Our Reports</Link>
+            <Link href={`${companyUrl}/#process`}>Process</Link>
+            <Link href={`${companyUrl}/blog/`}>Blog</Link>
+          </div>
+          <div>
+            {/* <ThemeToggle /> */}
+            <Button
+              onClick={() => window.open(companyUrl, '_blank')}
+              size={'2'}
+            >
+              Get in touch
+            </Button>
+          </div>
+        </div>
+        <div className="text-center pt-8 md:pt-[64px] px-6 flex flex-col gap-4 md:gap-6">
+          <h1
+            className="text-xs md:text-base text-primary font-bold"
+            style={{ letterSpacing: 2 }}
+          >
+            ANTELOPE CHATBOT
+          </h1>
+          <h2 className="text-xl md:text-3xl font-bold">{title?.title}</h2>
+          <p className="text-[#B9CAD0] text-sm md:text-lg">
+            {title?.description}
+          </p>
+        </div>
+      </header>
     </>
   )
 }

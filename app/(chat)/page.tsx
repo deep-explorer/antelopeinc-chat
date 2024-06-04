fbe6a26a00d212d60ed4cdf341fa7e1ac4b6ab43
deep-explorer <explorer0933@gmail.com>
2024-06-04 09:39:51 +0200

    update: home for a chatbot list

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -2,6 +2,7 @@ import { nanoid } from '@/lib/utils'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '../actions'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Antelope Chatbot'
@@ -12,5 +13,24 @@ export default async function IndexPage() {
   // const session = (await auth()) as Session
   // const missingKeys = await getMissingKeys()
 
-  return <p>Select an analyzer</p>
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="text-xl font-semibold">Select an analyzer</p>
+      <Link href="/renzos" className="italic hover:underline text-primary">
+        Renzos
+      </Link>
+      <Link
+        href="/tools/linkedin-analyzer"
+        className="italic hover:underline text-primary"
+      >
+        LinkedIn Analyzer
+      </Link>
+      <Link
+        href="/tools/content-intelligence"
+        className="italic hover:underline text-primary"
+      >
+        Content Intelligence
+      </Link>
+    </div>
+  )
 }

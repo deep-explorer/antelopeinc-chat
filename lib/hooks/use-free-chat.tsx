68250e1a5d4b181ea11f99138d04f79679bf5c02
deep-explorer <explorer0933@gmail.com>
2024-06-11 07:53:31 -0700

    reduced redundant code

--- lib/hooks/use-free-chat.tsx
+++ lib/hooks/use-free-chat.tsx
@@ -68,7 +68,7 @@ export function FreeChatProvider({ children }: FreeChatProviderProps) {
   const setBypassMode = (flag: boolean) => {
     if (ENVIRONMENT === 'development') {
       _setBypassMode(flag)
-      setEmailVerified(flag ? true : false)
+      setEmailVerified(flag)
     } else {
       console.warn(
         'This is a production environment. Bypass mode is not allowed.'

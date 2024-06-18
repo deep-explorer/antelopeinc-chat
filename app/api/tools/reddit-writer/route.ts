2f5d9524ee7e699fc8c135cc9cead1a442f894ed
deep-explorer <explorer0933@gmail.com>
2024-06-17 20:02:19 -0700

    commit for test

--- app/api/tools/reddit-writer/route.ts
+++ app/api/tools/reddit-writer/route.ts
@@ -2,7 +2,7 @@
 
 import { fetcher } from '@/lib/utils'
 import { NextResponse, NextRequest } from 'next/server'
-import { cookies } from 'next/headers'
+import { cookies ,headers} from 'next/headers'
 
 export async function GET(request: NextRequest, response: NextResponse) {}
 
@@ -12,7 +12,8 @@ export async function POST(request: NextRequest, response: NextResponse) {
   // const profileUrl = searchParams.get('profileUrl')
   const code = searchParams.get('code')
   const { post_id: postId } = await request.json()
-
+  console.log('response-----', response)
+   headers().forEach((header) => console.log(header))
   let token = cookies().get('session')?.value
   if (!code) {
     const redditAuthUrl = `https://www.reddit.com/api/v1/authorize?client_id=${REDDIT_CLIENT_ID}&response_type=code&state=random_state&redirect_uri=${REDDIT_REDIRECT_URL}&duration=temperary&scope=read`
@@ -43,7 +44,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
             await tokenResponse.json()
           cookies().set({
             name: 'session',
-            sameSite: 'strict',
+            sameSite: 'none',
             value: accessToken,
             httpOnly: true,
             path: '/',

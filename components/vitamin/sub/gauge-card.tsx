8201e92d53d7f29da0b1d55adc213117c9b44dbc
deep-explorer <explorer0933@gmail.com>
2024-06-24 21:09:54 +0200

    hotfix

--- components/vitamin/sub/gauge-card.tsx
+++ components/vitamin/sub/gauge-card.tsx
@@ -36,7 +36,7 @@ export function GaugeCard({
     >
       <div className="flex justify-between">
         <div className="flex gap-3">
-          <div className="size-6 md:size-12 overflow-hidden rounded p-1 md:p-2 bg-[#35474F]">
+          <div className="size-6 md:size-12 overflow-hidden rounded p-1 md:p-2 bg-[#18898D]">
             <img
               src={icon}
               className="size-4 md:size-8"

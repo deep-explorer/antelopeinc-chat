aa5f664c8209c38403b2168f170267a2be13686e
deep-explorer <explorer0933@gmail.com>
2024-07-01 22:31:23 +0200

    test

--- components/vitamin/sub/map-chart.tsx
+++ components/vitamin/sub/map-chart.tsx
@@ -1,76 +1,76 @@
 import { IBasicElement } from '@/components/content-template'
-import { PrimaryTooltip } from '@/components/ui/tooltip'
-import { InfoCircledIcon } from '@radix-ui/react-icons'
-import Image from 'next/image'
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-  Label
-} from 'recharts'
-import { ScatterCustomizedShape } from 'recharts/types/cartesian/Scatter'
-import { useWindowSize } from 'usehooks-ts'
+// import { PrimaryTooltip } from '@/components/ui/tooltip'
+// import { InfoCircledIcon } from '@radix-ui/react-icons'
+// import Image from 'next/image'
+// import {
+//   ScatterChart,
+//   Scatter,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   ResponsiveContainer,
+//   Label
+// } from 'recharts'
+// import { ScatterCustomizedShape } from 'recharts/types/cartesian/Scatter'
+// import { useWindowSize } from 'usehooks-ts'
 
-const data = [
-  { name: 'Other1', x: 10, y: 60 },
-  { name: 'Renzos', x: 20, y: 20 },
-  {
-    name: 'Other2',
-    x: 60,
-    y: 60
-  },
-  {
-    name: 'Other3',
-    x: 75,
-    y: 93
-  },
-  {
-    name: 'Other4',
-    x: 88,
-    y: 17
-  }
-]
+// const data = [
+//   { name: 'Other1', x: 10, y: 60 },
+//   { name: 'Renzos', x: 20, y: 20 },
+//   {
+//     name: 'Other2',
+//     x: 60,
+//     y: 60
+//   },
+//   {
+//     name: 'Other3',
+//     x: 75,
+//     y: 93
+//   },
+//   {
+//     name: 'Other4',
+//     x: 88,
+//     y: 17
+//   }
+// ]
 
-const renderCustomShape = (props: {
-  cx: number
-  cy: number
-  payload: any
-}): JSX.Element => {
-  const { cx, cy } = props
+// const renderCustomShape = (props: {
+//   cx: number
+//   cy: number
+//   payload: any
+// }): JSX.Element => {
+//   const { cx, cy } = props
 
-  return (
-    <>
-      <defs>
-        <clipPath id={`round${props.payload.key}`}>
-          <circle cx={cx} cy={cy} r={window.innerWidth > 768 ? 24 : 12} />
-        </clipPath>
-      </defs>
-      <image
-        href={`${props.payload.logo}`}
-        x={cx - (window.innerWidth > 768 ? 24 : 12)}
-        y={cy - (window.innerWidth > 768 ? 24 : 12)}
-        className="size-6 md:size-12"
-        clipPath={`url(#round${props.payload.key})`}
-      />
-    </>
-  )
-}
+//   return (
+//     <>
+//       <defs>
+//         <clipPath id={`round${props.payload.key}`}>
+//           <circle cx={cx} cy={cy} r={window.innerWidth > 768 ? 24 : 12} />
+//         </clipPath>
+//       </defs>
+//       <image
+//         href={`${props.payload.logo}`}
+//         x={cx - (window.innerWidth > 768 ? 24 : 12)}
+//         y={cy - (window.innerWidth > 768 ? 24 : 12)}
+//         className="size-6 md:size-12"
+//         clipPath={`url(#round${props.payload.key})`}
+//       />
+//     </>
+//   )
+// }
 
-const CustomTooltip: React.FC<any> = ({ active, payload }) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className="bg-primary rounded-md p-2">
-        <p className="text-[10px] md:text-[14px]">{payload[0].payload.name}</p>
-      </div>
-    )
-  }
+// const CustomTooltip: React.FC<any> = ({ active, payload }) => {
+//   if (active && payload && payload.length) {
+//     return (
+//       <div className="bg-primary rounded-md p-2">
+//         <p className="text-[10px] md:text-[14px]">{payload[0].payload.name}</p>
+//       </div>
+//     )
+//   }
 
-  return null
-}
+//   return null
+// }
 
 export interface IMapChartProps extends IBasicElement {
   type: 'map'
@@ -91,128 +91,246 @@ interface IMapElements {
     size: number //   ?
   }
 }
-export function MapChart({
-  title,
-  tooltip,
-  axes,
-  icon,
-  children,
-  className
-}: IMapChartProps) {
-  const { width: windowWidth } = useWindowSize()
+// export function MapChart({
+//   title,
+//   tooltip,
+//   axes,
+//   icon,
+//   children,
+//   className
+// }: IMapChartProps) {
+//   const { width: windowWidth } = useWindowSize()
 
-  return (
-    <div
-      className={`w-[260px] md:w-[605px] rounded-md bg-[#1E333B] flex flex-col gap-3 md:gap-6 p-4 md:p-8 ${className}`}
-    >
-      <div className="flex justify-between">
-        <div className="flex gap-2">
-          <img
-            src={icon}
-            alt={`/image-icons/${icon}.png`}
-            className="rounded-md size-6 md:size-12"
-          />
-          <h2 className="text-lg md:text-xl font-bold self-center">{title}</h2>
-        </div>
-        <PrimaryTooltip description={tooltip} />
+//   return (
+//     <div
+//       className={`w-[260px] md:w-[605px] rounded-md bg-[#1E333B] flex flex-col gap-3 md:gap-6 p-4 md:p-8 ${className}`}
+//     >
+//       <div className="flex justify-between">
+//         <div className="flex gap-2">
+//           <img
+//             src={icon}
+//             alt={`/image-icons/${icon}.png`}
+//             className="rounded-md size-6 md:size-12"
+//           />
+//           <h2 className="text-lg md:text-xl font-bold self-center">{title}</h2>
+//         </div>
+//         <PrimaryTooltip description={tooltip} />
+//       </div>
+//       <p className="text-xs md:text-lg">
+//         {/* TODO: description from Props */}
+//         {title} engagement metrics suggest a highly active and loyal community,
+//         with users resonating strongly with visually-driven storytelling and
+//         behind-the-scenes content.
+//       </p>
+//       <ResponsiveContainer width="100%" height={windowWidth > 768 ? 400 : 200}>
+//         <ScatterChart>
+//           <CartesianGrid
+//             strokeOpacity={0.1}
+//             // enableBackground={'/image-icons/invalid.png'}
+//           />
+//           <XAxis
+//             type="number"
+//             dataKey="x"
+//             domain={[0, 100]}
+//             padding={{
+//               left: windowWidth > 768 ? 20 : 8
+//             }}
+//             tickFormatter={(value, index) => {
+//               if (value === 0) return 'Few'
+//               if (value === 100) return 'Many'
+//               return ''
+//             }}
+//             style={{
+//               fontSize: windowWidth > 768 ? 14 : 10
+//             }}
+//           >
+//             <Label
+//               value={axes.x}
+//               offset={10}
+//               position="insideBottom"
+//               className="fill-white"
+//               style={{
+//                 textAnchor: 'middle',
+//                 letterSpacing: '1px',
+//                 fontSize: windowWidth > 768 ? 14 : 10
+//               }}
+//             />
+//           </XAxis>
+//           <YAxis
+//             type="number"
+//             dataKey="y"
+//             domain={[0, 100]}
+//             padding={{
+//               bottom: windowWidth > 768 ? 20 : 8
+//             }}
+//             tickFormatter={(value, index) => {
+//               if (value === 0) return 'Low'
+//               if (value === 100) return 'High'
+//               return ''
+//             }}
+//             style={{
+//               fontSize: windowWidth > 768 ? 14 : 10
+//             }}
+//           >
+//             <Label
+//               value={axes.y}
+//               offset={40}
+//               angle={-90}
+//               position="insideLeft"
+//               className="fill-white"
+//               style={{
+//                 textAnchor: 'middle',
+//                 letterSpacing: '1px',
+//                 fontSize: windowWidth > 768 ? 14 : 10
+//               }}
+//             />
+//           </YAxis>
+//           <Tooltip
+//             cursor={{ strokeDasharray: '3 3' }}
+//             content={CustomTooltip}
+//           />
+//           <Scatter
+//             name="A school"
+//             data={Object.keys(children).map((key, index) => {
+//               const maxX = Math.max(
+//                 ...Object.values(children).map(child => child.x)
+//               )
+//               const maxY = Math.max(
+//                 ...Object.values(children).map(child => child.y)
+//               )
+
+//               return {
+//                 name: key,
+//                 x: (Math.log(children[key].x + 1) / Math.log(maxX + 1)) * 90, //  maxX equals to 90 out of 100
+//                 y: (Math.log(children[key].y + 1) / Math.log(maxY + 1)) * 90,
+//                 logo: children[key].logo,
+//                 tooltip: children[key].tooltip,
+//                 size: children[key].size,
+//                 key: title + index
+//               }
+//             })}
+//             shape={renderCustomShape as ScatterCustomizedShape}
+//           />
+//         </ScatterChart>
+//       </ResponsiveContainer>
+//     </div>
+//   )
+// }
+
+import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+]
+
+const getIntroOfPage = (label: string) => {
+  if (label === 'Page A') {
+    return "Page A is about men's clothing"
+  }
+  if (label === 'Page B') {
+    return "Page B is about women's dress"
+  }
+  if (label === 'Page C') {
+    return "Page C is about women's bag"
+  }
+  if (label === 'Page D') {
+    return 'Page D is about household goods'
+  }
+  if (label === 'Page E') {
+    return 'Page E is about food'
+  }
+  if (label === 'Page F') {
+    return 'Page F is about baby food'
+  }
+  return ''
+}
+
+const CustomTooltip = ({ active, payload, label }: any) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="intro">{getIntroOfPage(label)}</p>
+        <p className="desc">Anything you want can be displayed here.</p>
       </div>
-      <p className="text-xs md:text-lg">
-        {/* TODO: description from Props */}
-        {title} engagement metrics suggest a highly active and loyal community,
-        with users resonating strongly with visually-driven storytelling and
-        behind-the-scenes content.
-      </p>
-      <ResponsiveContainer width="100%" height={windowWidth > 768 ? 400 : 200}>
-        <ScatterChart>
-          <CartesianGrid
-            strokeOpacity={0.1}
-            // enableBackground={'/image-icons/invalid.png'}
-          />
-          <XAxis
-            type="number"
-            dataKey="x"
-            domain={[0, 100]}
-            padding={{
-              left: windowWidth > 768 ? 20 : 8
-            }}
-            tickFormatter={(value, index) => {
-              if (value === 0) return 'Few'
-              if (value === 100) return 'Many'
-              return ''
-            }}
-            style={{
-              fontSize: windowWidth > 768 ? 14 : 10
-            }}
-          >
-            <Label
-              value={axes.x}
-              offset={10}
-              position="insideBottom"
-              className="fill-white"
-              style={{
-                textAnchor: 'middle',
-                letterSpacing: '1px',
-                fontSize: windowWidth > 768 ? 14 : 10
-              }}
-            />
-          </XAxis>
-          <YAxis
-            type="number"
-            dataKey="y"
-            domain={[0, 100]}
-            padding={{
-              bottom: windowWidth > 768 ? 20 : 8
-            }}
-            tickFormatter={(value, index) => {
-              if (value === 0) return 'Low'
-              if (value === 100) return 'High'
-              return ''
-            }}
-            style={{
-              fontSize: windowWidth > 768 ? 14 : 10
-            }}
-          >
-            <Label
-              value={axes.y}
-              offset={40}
-              angle={-90}
-              position="insideLeft"
-              className="fill-white"
-              style={{
-                textAnchor: 'middle',
-                letterSpacing: '1px',
-                fontSize: windowWidth > 768 ? 14 : 10
-              }}
-            />
-          </YAxis>
-          <Tooltip
-            cursor={{ strokeDasharray: '3 3' }}
-            content={CustomTooltip}
-          />
-          <Scatter
-            name="A school"
-            data={Object.keys(children).map((key, index) => {
-              const maxX = Math.max(
-                ...Object.values(children).map(child => child.x)
-              )
-              const maxY = Math.max(
-                ...Object.values(children).map(child => child.y)
-              )
+    )
+  }
 
-              return {
-                name: key,
-                x: (Math.log(children[key].x + 1) / Math.log(maxX + 1)) * 90, //  maxX equals to 90 out of 100
-                y: (Math.log(children[key].y + 1) / Math.log(maxY + 1)) * 90,
-                logo: children[key].logo,
-                tooltip: children[key].tooltip,
-                size: children[key].size,
-                key: title + index
-              }
-            })}
-            shape={renderCustomShape as ScatterCustomizedShape}
-          />
-        </ScatterChart>
-      </ResponsiveContainer>
-    </div>
+  return null
+}
+
+export const MapChart = (p: any) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <Bar dataKey="pv" barSize={20} fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }

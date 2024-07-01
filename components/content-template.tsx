94f2465ffbeefa64741d55924319cb1c6f8ee91c
deep-explorer <explorer0933@gmail.com>
2024-07-01 22:21:41 +0200

    hotfix

--- components/content-template.tsx
+++ components/content-template.tsx
@@ -178,7 +178,7 @@ export const ElementsWrapper = ({
     case 'slider':
       return (
         // <Carousel onChange={progress => setCarouselProgress(progress)}>
-        <div>{children}</div>
+        <div>Slider{children}</div>
         // </Carousel>
       )
     case 'accordion':

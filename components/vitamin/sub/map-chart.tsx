631ed0e5273451eb5a24510daa233203ae69221a
deep-explorer <explorer0933@gmail.com>
2024-07-01 22:03:59 +0200

    test mobile tap

--- components/vitamin/sub/map-chart.tsx
+++ components/vitamin/sub/map-chart.tsx
@@ -312,25 +312,23 @@ const CustomTooltip = ({ active, payload, label }: any) => {
 
 export function MapChart(props: any) {
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip content={<CustomTooltip />} />
-        <Legend />
-        <Bar dataKey="pv" barSize={20} fill="#8884d8" />
-      </BarChart>
-    </ResponsiveContainer>
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip content={<CustomTooltip />} />
+      <Legend />
+      <Bar dataKey="pv" barSize={20} fill="#8884d8" />
+    </BarChart>
   )
 }
